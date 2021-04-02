6f858c76848cf42bf001ec0f4be93f17a1465d0e
= <jackos@me.com>
2021-04-03 07:58:21 +0800

    Breakpoint fixes

--- src/lib/HomePage.svelte
+++ src/lib/HomePage.svelte
@@ -1,18 +1,22 @@
-<div class="w-full mt-20 flex items-center justify-center bg-color">
+<script>
+	import { display } from './../stores.js';
+</script>
+
+<div class="w-full md:mt-20 flex items-center justify-center bg-color">
 	<div class="md:w-2/3 w-full px-4 text-color flex flex-col">
-		<div class="w-full text-7xl font-bold">
-			<h1 class="w-full md:w-3">Rapid Development</h1>
+		<div class="w-full md:text-7xl text-xl font-bold">
+			<h1 class="w-full md:w-3">Rapid Iteration</h1>
 			<h1 class="w-full md:w-2/3">Unlimited Scalability</h1>
 		</div>
 		<div class="flex mt-8 flex-col md:flex-row md:justify-between">
 			<p class="w-full md:w-2/3 text-color">
-				Get your AI web apps to market in record speed using 100% horizontally scalable
-				technologies, ready for when your business booms.
+				Get your AI web apps to market in record speed using technologies built for scale, ready for
+				when your business booms.
 			</p>
-			<div class="w-44 pt-6 md:pt-0">
+			<div class="w-44 pt-6 md:pl-8 md:pt-0">
 				<a
 					href="mailto:mdldotai@gmail.com"
-					class="bg-indigo-500 text-gray-800 justify-center text-center rounded-lg shadow px-10 py-3 flex items-center"
+					class="bg-indigo-500 text-gray-800 justify-center text-center rounded-lg md:flex shadow px-4 py-3 items-center"
 					>Contact us
 				</a>
 			</div>
--- src/lib/Sidebar.svelte
+++ src/lib/Sidebar.svelte
@@ -1,6 +1,7 @@
 <script>
 	import LogoSmall from './../svg/LogoSmall.svelte';
 	import Logo from '../svg/Logo.svelte';
+	import Logo2 from '../svg/Logo2.svelte';
 	import Branch from './../svg/Branch.svelte';
 	import Home from './../svg/Home.svelte';
 	import Stack from '../svg/Stack.svelte';
@@ -12,33 +13,31 @@
 		<div class="flex flex-col ">
 			<div class="flex flex-col flex-grow pt-5 pb-4 overflow-y-auto">
 				{#if $display.sidebar}
-					<div class="flex items-center flex-shrink-0 px-4">
-						<Logo />
+					<div class="px-5 pb-4">
+						<Logo2 />
 					</div>
-					<div class="flex flex-col flex-1 mt-5">
-						<nav class="flex-1 px-2 space-y-1">
-							<button
-								on:click={() => ($display.main = 'home')}
-								class={$display.main === 'home' ? 'selected' : 'btn'}
-							>
-								<Home /> Home
-							</button>
-							<button
-								on:click={() => ($display.main = 'tech')}
-								class={$display.main === 'tech' ? 'selected' : 'btn'}
-							>
-								<Stack />
-								Technologies
-							</button>
-							<button
-								on:click={() => ($display.main = 'models')}
-								class={$display.main === 'models' ? 'selected' : 'btn'}
-							>
-								<Branch />
-								Models
-							</button>
-						</nav>
-					</div>
+					<nav class="flex-1 px-2 space-y-1">
+						<button
+							on:click={() => ($display.main = 'home')}
+							class={$display.main === 'home' ? 'selected' : 'btn'}
+						>
+							<Home /> Home
+						</button>
+						<button
+							on:click={() => ($display.main = 'tech')}
+							class={$display.main === 'tech' ? 'selected' : 'btn'}
+						>
+							<Stack />
+							Technologies
+						</button>
+						<button
+							on:click={() => ($display.main = 'models')}
+							class={$display.main === 'models' ? 'selected' : 'btn'}
+						>
+							<Branch />
+							Models
+						</button>
+					</nav>
 				{:else}
 					<div class="pl-5 pb-4">
 						<LogoSmall />
